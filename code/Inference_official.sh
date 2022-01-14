88112fae5b36a70177535d436df87f48e6c1f08f
ShangWei <43960503+shangwei5@users.noreply.github.com>
2022-01-15 00:02:29 +0800

    Add files via upload

---
+++ code/Inference_official.sh
@@ -0,0 +1,7 @@
+#! bash
+
+# Reconstruct without event data
+python inference_d2net_off.py --data_path '/your/path/to/data'
+
+# Reconstruct with event data
+#python inference_d2net_off_event.py   --data_path  '/your/path/to/data'
